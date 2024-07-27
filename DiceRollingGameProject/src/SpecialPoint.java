72651b075f7b486df22b4d5bb9c67587cc71262b
Menes <Menes.1905>
2024-07-28 02:20:04 +0300

    SpecialPoint class comment added

--- DiceRollingGameProject/src/SpecialPoint.java
+++ DiceRollingGameProject/src/SpecialPoint.java
@@ -4,6 +4,8 @@ public class SpecialPoint implements Point {
     @Override
     public void addPoint(ArrayList<Player> players, int[] points) {
 
+        // if only two players get same point and this point is greater than  point that other player gets ,
+        // then add to  point multiplied by 2  to  players who get same point and add the point other player get to other player
         if(points[0] == points[1] ){
             players.get(0).addPoint(points[0] * 2);
             players.get(1).addPoint(points[1] * 2);
